34b19b5e6cfa027ce70636aac312417ed1507cb0
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-09-21 19:09:48 -0400

    Update Inertial.py

--- Inertial.py
+++ Inertial.py
@@ -143,7 +143,7 @@ class InertialSensor():
 		bus.write_byte_data(MPU6050_ADDR, PWR_MGMT_1, 0x01)
 		time.sleep(0.1)
 		#Write to Configuration register
-		bus.write_byte_data(MPU6050_ADDR, CONFIG, 0)
+		bus.write_byte_data(MPU6050_ADDR, CONFIG, int('0000110',2))
 		time.sleep(0.1)
 		#Write to Gyro configuration register
 		gyro_config_sel = [0b00000,0b010000,0b10000,0b11000] # byte registers
